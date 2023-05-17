e26221f3a70a438b8e1b9ef36074126ba0fca859
Mari Cornelio <katherinacornelio@gmail.com>
2023-05-17 10:47:07 -0500

    Database creation   - Adding products   - Adding images in images folder   - Adding stores

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kwai Store Server Mock
+
+Server Mock para el proyecto [kwai Store](https://github.com/MariCornelio/KwaiStore)
+
+Usando la librería [json-server](https://github.com/typicode/json-server)
+
+## Ejecución
+
+1. Clonar el repositorio
+2. Instalar las dependencias
+   ```bash
+   npm install
+   ```
+3. Ejecutar el mock
+   ```bash
+   npm start
+   ```
---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const jsonServer = require('json-server');
+
+const middlewares = jsonServer.defaults()
+
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+const port = process.env.PORT || 8080;
+
+
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(middlewares);
+
+app.use(router);
+app.listen(port, () => {
+  console.log(`JSON Server is running in ${port}`);
+});
